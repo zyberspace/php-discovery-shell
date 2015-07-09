a5b4bb268c06e2cafb25e0b0383435bf448263a8
zyberspace <zyberspace@zyberware.org>
2015-07-10 00:50:13 +0200

    Added license

--- lib/Zyberspace/DiscoveryShell.php
+++ lib/Zyberspace/DiscoveryShell.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Copyright 2015 Eric Enold <zyberspace@zyberware.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 namespace Zyberspace;
 
 class DiscoveryShell
